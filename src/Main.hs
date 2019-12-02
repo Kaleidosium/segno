6a7c88e91d830feca921e83489334821e47051af
WorldSEnder <WorldSEnder@users.noreply.github.com>
2019-12-02 07:41:53 +0000

    Suggested edits to improve code quality (#1)
    * added stack tooling
    
    * Main should be more readable now

--- src/Main.hs
+++ src/Main.hs
@@ -26,55 +26,49 @@ module Main where
 
 import Codec.Midi
 import Control.Monad
+import Data.Maybe
 import Data.List.Split
 import Data.Typeable
 import Prelude
 import Segno.Music
 import System.IO
 
-data GMain =
-  GMain { tokens :: [[String]] }
-  deriving (Show, Eq)
-
-tup f1 f2 f3 =
-  foldr
-    (\t trs ->
-       case t of
-         (a:[])     -> f1 a trs
-         (a:b:[])   -> f2 a b trs
-         (a:b:c:[]) -> f3 a b c trs
-         _          -> "?" : trs)
-    []
-
-one = tup (\a trs -> a : trs) (\a _ trs -> a : trs) (\a _ _ trs -> a : trs)
-
-two = tup (\_ trs -> trs) (\_ b trs -> b : trs) (\_ b _ trs -> b : trs)
-
-thr = tup (\_ trs -> trs) (\_ _ trs -> trs) (\_ _ c trs -> c : trs)
+data ChannelData = Channel1 String | Channel2 String | Channel3 String
+    deriving (Show, Eq)
 
 codecMulti n =
-  Midi {fileType = MultiTrack, timeDiv = TicksPerBeat 24, Codec.Midi.tracks = n}
+  Midi {fileType = MultiTrack, timeDiv = TicksPerBeat 24, tracks = n}
 
-looping (GMain y) = do
-  allX@(x:xs) <- getLine
-  case x of
-    'q' -> do
-      let trs@(tr1, tr2, tr3) = (one y, two y, thr y)
-      putStr "Channel 1> "
-      print tr1
-      putStr "Channel 2> "
-      print tr2
-      putStr "Channel 3> "
-      print tr3
-      let m1 = (foldr (\x a -> (notes 1 x) ++ a) [] tr1)
-      let m2 = (foldr (\x a -> (notes 2 x) ++ a) [] tr2)
-      let m3 = (foldr (\x a -> (notes 3 x) ++ a) [] tr3)
-      exportFile "output.mid" (codecMulti [m1, m2, m3])
-    _ -> readLine y allX
+dumpToFile input = do
+  let tr1 = [ a | Channel1 a <- input ]
+  let tr2 = [ a | Channel2 a <- input ]
+  let tr3 = [ a | Channel3 a <- input ]
+  putStr "Channel 1> "
+  print tr1
+  putStr "Channel 2> "
+  print tr2
+  putStr "Channel 3> "
+  print tr3
+  let m1 = tr1 >>= notes 1
+  let m2 = tr2 >>= notes 2
+  let m3 = tr3 >>= notes 3
+  exportFile "output.mid" (codecMulti [m1, m2, m3])
 
-readLine y x = do
-  let f = filter (/= "") (splitOn " " x)
-  looping (GMain (y ++ [f]))
+parseInput [a]       = return $ Just $ [Channel1 a]
+parseInput [a, b]    = return $ Just $ [Channel1 a, Channel2 b]
+parseInput [a, b, c] = return $ Just $ [Channel1 a, Channel2 b, Channel3 c]
+parseInput _         = putStrLn "Error: Unrecognized input!" >> return Nothing
+
+looping revinput = do
+  allX <- getLine
+  if not (null allX) && head allX == 'q'
+  then dumpToFile (reverse revinput)
+  else
+    do f <- parseInput $ filter (/= "") (splitOn " " allX)
+       case f of
+         Just f ->  looping (f ++ revinput)
+         Nothing -> looping revinput
 
 main :: IO ()
-main = looping (GMain [])
+main = looping []
+
