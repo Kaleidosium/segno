70d4ab95c089db44ab61bf0f2b6b9fab4f1f3614
iamrifki <iamrifki0@gmail.com>
2019-12-03 09:40:57 +0700

    Commit broken.

--- src/Segno/Music.hs
+++ src/Segno/Music.hs
@@ -1,25 +1,55 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Segno.Music where
 
 import Codec.Midi
+import Text.Parsec
 import Control.Applicative
 import Prelude
 
 type TrackNumber = Int
 
-notes :: TrackNumber -> String -> [(Ticks, Message)]
-notes c ('>':s) = [(0, Copyright s)]
-notes c "pause" = [(0, NoteOn c 60 0), (24, NoteOff c 60 0)]
-notes c ('#':s) = [(0, Text s)]
-notes c "end" = [(0, TrackEnd)]
-notes c ('l':s) = lowNote <$> notes c s
-notes c ('h':s) = lowNote <$> notes c s
-notes c [l, o, '-'] = halfNote <$> notes c [l, o]
-notes c [l, o, '_'] = halfNote . halfNote <$> notes c [l, o]
-notes c [l, o] =
+element c = choice [ copyright
+                   , pause c
+                   , end
+                   , note c
+                   , text
+                   ]
+
+copyright = do
+  char '>'
+  s <- anyChar `manyTill` newline
+  return [(0, Copyright s)]
+
+pause c = do
+  string "pause"
+  return [(0, NoteOn c 60 0), (24, NoteOff c 60 0)]
+
+end = do
+  string "end"
+  return [(0, TrackEnd)]
+
+text = do
+  Text.Parsec.optional $ char '#'
+  s <- anyChar `manyTill` newline
+  return [(0, Text s)]
+
+note c = do
+  a <- optionMaybe $ oneOf "lh"
+  l <- oneOf "cdefgab"
+  o <- oneOf "01234"
+  h <- optionMaybe $ oneOf "-_"
   let m = (case o of '0' -> 48; '1' -> 60; '2' -> 72; '3' -> 84; '4' -> 96; _ -> 60)
         + (case l of 'c' -> 0; 'd' -> 2; 'e' -> 4; 'f' -> 5; 'g' -> 7; 'a' -> 9; 'b' -> 11; _ -> 0)
-  in [(0, NoteOn c m 80), (24, NoteOff c m 0)]
-notes c s = [(0, Text s)]
+  let f1 = case a of
+            Just 'l' -> lowNote
+            Just 'h' -> highNote
+            Nothing  -> id
+  let f2 = case h of
+            Just '-' -> halfNote
+            Just '_' -> halfNote . halfNote
+            Nothing  -> id
+  return $ f1 . f2 <$> [(0, NoteOn c m 80), (24, NoteOff c m 0)]
 
 lowNote :: (Ticks, Message) -> (Ticks, Message)
 lowNote (d, NoteOn x m v)  = (d, NoteOn x (m - 1) v)
@@ -34,3 +64,6 @@ highNote m                  = m
 halfNote :: (Ticks, Message) -> (Ticks, Message)
 halfNote (d, NoteOff x m v) = (d + d, NoteOff x m v)
 halfNote m                  = m
+
+notes :: TrackNumber -> String -> Either ParseError [(Ticks, Message)]
+notes c s = parse (element c) "" s
